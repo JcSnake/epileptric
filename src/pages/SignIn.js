4e9623e27b0845f5297f7d5e1fef06aed87606bd
Len Serrano <len.serrano.g@gmail.com>
2019-12-10 13:34:16 -0600

    l: estructura de forms sign in/up

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../assets/sass/forms.scss';
 
 class SignIn extends React.Component{
     render(){
         return(
             <>
-            <h1>Sign In</h1>
+                <div className = "container-form">
+                    <form action="">
+                        <input type="email" name="" id="" className="input-sign" placeholder = "E-mail" />
+                        <input type="password" name="" id="" className="input-sign" placeholder = "Password" />
+                        <div className="container-button">
+                            <button className = "btn-form">Sign In</button>
+                            <span>
+                                Don't have an account? 
+                                <Link className = "link-form" to = "/signup"> Create one</Link>
+                            </span>
+                        </div>
+                    </form>
+                </div>
             </>
         );
     }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,39 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 class SignUp extends React.Component{
     render(){
         return(
             <>
-            <h1>Sign Up</h1>
+                <div className = "container-form">
+                    <form action="">
+                        <label>First Name: </label>
+                            <input type="text" name="firstName" id="" className="input-sign" placeholder = "Name" />
+                        <label>Last Name: </label>
+                            <input type="text" name="lastNameP" id="" className="input-sign" placeholder = "Last Name" />
+                            <input type="text" name="lastNameM" id="" className="input-sign" placeholder = "Last Name" />
+                        <label>Birthday: </label>
+                            <input 
+                                type="date" 
+                                name="birthday" 
+                                id="" 
+                                className = "input-sign"
+                                min = "01/01/1930"
+                            />
+                        <label>Genre: </label>
+                            <input list="genres" className="input-sign" name="genre" />
+                                <datalist id="genres">
+                                    <option value="Female"> Female </option>
+                                    <option value="Male"> Male </option>
+                                </datalist>
+                        <div className="container-button">
+                            <button className = "btn-form">Sign Up</button>
+                            <span>
+                                Already have an account? 
+                                <Link className = "link-form" to = "/signup"> Sign In</Link>
+                            </span>
+                        </div>
+                    </form>
+                </div>
             </>
         );
     }
